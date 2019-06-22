ecee2ff2877d4c92f4a0738ccc86f66cc1ec7045
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2019-06-21 21:12:01 -0300

    x

---
+++ var_loop.js
@@ -0,0 +1,9 @@
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log("valor de i é ", i)
+
+/**
+ * Mesmo o var sendo declarado dentro do for, ele pode ser visto fora dele
+*/ 
---
+++ var_loop2.js
@@ -0,0 +1,9 @@
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log("valor de i é ", i)
+
+/**
+ * No caso o let só pega o o que esta dentro do for
+*/ 
