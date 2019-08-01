3cf8054703da7277b1de270ce52b559d38747ece
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2019-08-01 13:39:08 -0300

    destructuring02

---
+++ destructuring01.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: "Felipe",
+    idade: 5,
+    endereco: {
+        Logradouro: "Rua ABC",
+        numero: 1000
+    }
+}
+
+const { nome, idade } = pessoa
+
+console.log(nome, idade)
